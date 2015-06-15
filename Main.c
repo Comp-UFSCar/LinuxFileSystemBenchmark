e3e38d49671d765a469f913029f01258d1e7390c
lucasmontec <lucas_carvalhaes@hotmail.com>
2015-06-15 18:14:39 -0300

    Added the start of the read code. Needs tweaking. Sequential read only (first).

--- Main.c
+++ Main.c
@@ -13,7 +13,9 @@
 
 //The number Bytes to write (2^SIZE)
 #define SIZE 16
-//The number Bytes to write in each req (2^REQSIZE)
+//The max number Bytes to write in each req (2^REQSIZE)
+//A req tests writing from 2^0 to 2^REQSIZE
+//All tests test each case using reqsize
 #define REQSIZE 16
 //Useful constants
 #define _64KB 65536
@@ -22,7 +24,7 @@
 //4GB = 4194304KB
 //64KB = 65536B
 
-char* makeChunkBuffer(int chunkSize) {
+char* makeChunkBuffer(int chunkSize, int initialize) {
 	char *BUFF;
 	BUFF = (char*) calloc(chunkSize, sizeof(char));
 
@@ -31,9 +33,11 @@ char* makeChunkBuffer(int chunkSize) {
 		exit(-1);
 	}
 
-	int i = 0;
-	for (i = 0; i < chunkSize; i++) {
-		BUFF[i] = '0';
+	if(initialize){
+		int i = 0;
+		for (i = 0; i < chunkSize; i++) {
+			BUFF[i] = '0';
+		}
 	}
 	return BUFF;
 }
@@ -64,15 +68,15 @@ int main(void) {
 	//Use N to write chuks and increase by req size
 	while (n < REQSIZE) {
 		//Calculate the size of the current chunk
-		int chunkSize = (int) powf(2.0, n);
+		const int chunkSize = (int) powf(2.0, n);
 		//Calculate how many times the chunk needs to be written to fill the total size
-		int times = totalSize / chunkSize;
+		const int times = totalSize / chunkSize;
 
 		//Debug
 		printf("SW of %d bytes %d times. (%d bytes)\n", chunkSize, times,
 				totalSize);
 		//Build the buffer
-		Buffer = makeChunkBuffer(chunkSize);
+		Buffer = makeChunkBuffer(chunkSize,1);
 
 		//Benchmark the start time
 		float startTime = getTime();
@@ -94,7 +98,7 @@ int main(void) {
 		//Debug
 		printf("Sequential write time: %f s\n", timeElapsed);
 
-		//Free current chunk buffer (we will get a bigger on of no one)
+		//Free current chunk buffer (we will get a bigger one)
 		free(Buffer);
 		//Rewind the file
 		lseek(file, 0, SEEK_SET);
@@ -105,15 +109,11 @@ int main(void) {
 	//Inform
 	puts("Sequential write complete.");
 
-	//Free the buffer
-	//free(Buffer);
-
 	//Reset n
 	n = 0;
 
 	//Movers
 	int pos = 0;
-	int end = totalSize;
 
 	//Inform
 	puts("Starting random write");
@@ -124,15 +124,15 @@ int main(void) {
 	////////////////////////////////////////////////
 	while (n < REQSIZE) {
 		//Calculate the size of the current chunk
-		int chunkSize = (int) powf(2.0, n);
+		const int chunkSize = (int) powf(2.0, n);
 		//Calculate how many times the chunk needs to be written to fill the total size
-		int times = totalSize / chunkSize;
+		const int times = totalSize / chunkSize;
 
 		//Debug
 		printf("RW of %d bytes %d times. (%d bytes)\n", chunkSize, times,
 				totalSize);
 		//Build the buffer
-		Buffer = makeChunkBuffer(chunkSize);
+		Buffer = makeChunkBuffer(chunkSize,1);
 
 		//Benchmark the start time
 		float startTime = getTime();
@@ -141,10 +141,10 @@ int main(void) {
 		int i = 0;
 		for (i = 0; i < times; i++) {
 			//Calculate position
-			if(i<=times/2){
-				pos = i*chunkSize;
-			}else{
-				pos = totalSize - ((i-(times/2))*chunkSize);
+			if (i <= times / 2) {
+				pos = i * chunkSize;
+			} else {
+				pos = totalSize - ((i - (times / 2)) * chunkSize);
 			}
 
 			//Go to position
@@ -164,7 +164,7 @@ int main(void) {
 		//Debug
 		printf("Random write time: %f s\n", timeElapsed);
 
-		//Free current chunk buffer (we will get a bigger on of no one)
+		//Free current chunk buffer (we will get a bigger one)
 		free(Buffer);
 		//Rewind the file
 		lseek(file, 0, SEEK_SET);
@@ -172,10 +172,80 @@ int main(void) {
 		n++;
 	}
 
-	//End
-	puts("Ended.");
+	//End inform
+	puts("Ended write.");
 
-	//Close the file
+	//Close the file for write
+	close(file);
+
+	//Open the read file
+	file = open("bench.txt", O_RDONLY);
+
+	//Inform
+	puts("Read start.");
+
+	////////////////////////////////////////////////
+	////            SEQUENTIAL READ             ////
+	////////////////////////////////////////////////
+	//Reset n counter
+	n = 0;
+	//Use N to read chuks and increase by req size
+	while (n < REQSIZE) {
+		//Calculate the size of the current chunk
+		const int chunkSize = (int) powf(2.0, n);
+		//Calculate how many times the chunk needs to be read to fill the total size
+		const int times = totalSize / chunkSize;
+
+		//Debug
+		printf("SR of %d bytes %d times. (%d bytes)\n", chunkSize, times,
+				totalSize);
+		//Build the buffer
+		Buffer = makeChunkBuffer(chunkSize, 0);
+
+		//Benchmark the start time
+		float startTime = getTime();
+
+		//Read each chunk to the file using read syscall
+		int i = 0;
+		for (i = 0; i < times; i++) {
+			/*
+			 *  On files that support seeking, the read operation commences at the
+			 *	current file offset, and the file offset is incremented by the number
+			 * 	of bytes read. (read man)
+			 */
+			while (read(file, &Buffer, chunkSize) > 0) {}
+		}
+
+		//Benchmark end time
+		float endTime = getTime();
+		float timeElapsed = endTime - startTime;
+
+		//Check buffer (should all be zeroes)
+		/*i = 0;
+		for (i = 0; i < chunkSize; i++) {
+			if (Buffer[i] != '0') {
+				puts("Buffer error. Non-zero returned on read.");
+				printf("Current chunk size: %d\n",chunkSize);
+				printf("Buffer: %s\n",Buffer);
+				break;
+			}
+		}*/
+
+		//Debug
+		printf("Sequential read time: %f s\n", timeElapsed);
+
+		//Free current chunk buffer (we will get a bigger one)
+		free(Buffer);
+		//Rewind the file
+		lseek(file, 0, SEEK_SET);
+		//Increase chunk size
+		n++;
+	}
+
+	//End inform
+	puts("Ended read.");
+
+	//Close the file for write
 	close(file);
 
 	//return correctly
