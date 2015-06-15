fbf0b1a41364f023d4c30ad17956001f9ad991c7
lucasmontec <lucas_carvalhaes@hotmail.com>
2015-06-15 17:39:14 -0300

    New write method tested (pseudorandom write using lseek).

--- Main.c
+++ Main.c
@@ -22,51 +22,55 @@
 //4GB = 4194304KB
 //64KB = 65536B
 
-char* makeChunkBuffer(int chunkSize){
+char* makeChunkBuffer(int chunkSize) {
 	char *BUFF;
-	BUFF = (char*) calloc (chunkSize,sizeof(char));
+	BUFF = (char*) calloc(chunkSize, sizeof(char));
 
-	if(BUFF == NULL){
+	if (BUFF == NULL) {
 		puts("Buffer allocation failed!");
 		exit(-1);
 	}
 
 	int i = 0;
-	for(i=0;i<chunkSize;i++){
+	for (i = 0; i < chunkSize; i++) {
 		BUFF[i] = '0';
 	}
 	return BUFF;
 }
 
-int main(void){
+int main(void) {
 
 	puts("Started.");
 
 	int file = open("bench.txt", O_WRONLY | O_CREAT, 0644);
 
 	//Check if the file opened correctly
-	if(file < 0){
+	if (file < 0) {
 		perror("open");
 		return -1; //Close the program
 	}
 
 	puts("File ready.");
 
+	////////////////////////////////////////////////
+	////            SEQUENTIAL WRITE            ////
+	////////////////////////////////////////////////
 	//Write all chunks
 	int n = 0;
 	//Calculate total size to write
-	int totalSize = (int)powf(2.0,SIZE);
+	const int totalSize = (int) powf(2.0, SIZE);
 	//The buffer to use for writing
 	char *Buffer;
 	//Use N to write chuks and increase by req size
-	while(n<REQSIZE){
+	while (n < REQSIZE) {
 		//Calculate the size of the current chunk
-		int chunkSize = (int)powf(2.0,n);
+		int chunkSize = (int) powf(2.0, n);
 		//Calculate how many times the chunk needs to be written to fill the total size
-		int times = totalSize/chunkSize;
+		int times = totalSize / chunkSize;
 
 		//Debug
-		printf("SW of %d bytes %d times. (%d bytes)\n",chunkSize, times, totalSize);
+		printf("SW of %d bytes %d times. (%d bytes)\n", chunkSize, times,
+				totalSize);
 		//Build the buffer
 		Buffer = makeChunkBuffer(chunkSize);
 
@@ -75,8 +79,8 @@ int main(void){
 
 		//Write each chunk to the file using write syscall
 		int i = 0;
-		for(i=0;i<times;i++){
-			if(write(file, &Buffer, chunkSize) < 0){
+		for (i = 0; i < times; i++) {
+			if (write(file, &Buffer, chunkSize) < 0) {
 				puts("Write error.");
 				perror("write");
 				return -1; //Failed to write
@@ -93,7 +97,77 @@ int main(void){
 		//Free current chunk buffer (we will get a bigger on of no one)
 		free(Buffer);
 		//Rewind the file
-		lseek(file,0, SEEK_SET);
+		lseek(file, 0, SEEK_SET);
+		//Increase chunk size
+		n++;
+	}
+
+	//Inform
+	puts("Sequential write complete.");
+
+	//Free the buffer
+	//free(Buffer);
+
+	//Reset n
+	n = 0;
+
+	//Movers
+	int pos = 0;
+	int end = totalSize;
+
+	//Inform
+	puts("Starting random write");
+
+	//Use N to write chuks and increase by req size
+	////////////////////////////////////////////////
+	////              RANDOM WRITE              ////
+	////////////////////////////////////////////////
+	while (n < REQSIZE) {
+		//Calculate the size of the current chunk
+		int chunkSize = (int) powf(2.0, n);
+		//Calculate how many times the chunk needs to be written to fill the total size
+		int times = totalSize / chunkSize;
+
+		//Debug
+		printf("RW of %d bytes %d times. (%d bytes)\n", chunkSize, times,
+				totalSize);
+		//Build the buffer
+		Buffer = makeChunkBuffer(chunkSize);
+
+		//Benchmark the start time
+		float startTime = getTime();
+
+		//Write each chunk to the file using write syscall
+		int i = 0;
+		for (i = 0; i < times; i++) {
+			//Calculate position
+			if(i<=times/2){
+				pos = i*chunkSize;
+			}else{
+				pos = totalSize - ((i-(times/2))*chunkSize);
+			}
+
+			//Go to position
+			lseek(file, pos, SEEK_SET);
+
+			if (write(file, &Buffer, chunkSize) < 0) {
+				puts("Write error.");
+				perror("write");
+				return -1; //Failed to write
+			}
+		}
+
+		//Benchmark end time
+		float endTime = getTime();
+		float timeElapsed = endTime - startTime;
+
+		//Debug
+		printf("Random write time: %f s\n", timeElapsed);
+
+		//Free current chunk buffer (we will get a bigger on of no one)
+		free(Buffer);
+		//Rewind the file
+		lseek(file, 0, SEEK_SET);
 		//Increase chunk size
 		n++;
 	}
